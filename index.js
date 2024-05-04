2734688f6e33f9816040258a20080ec98e0105ce
Joseph Ruff <joegruff16@gmail.com>
2024-05-04 18:05:14 -0400

    Added questions to ask when user is prompted to add the data to their README, added module to package json

--- index.js
+++ index.js
@@ -1,13 +1,65 @@
+// How do these functions break down and how can I make them simple and small
+
 // TODO: Include packages needed for this application
+// This can be developed by reading the inquire documentation
+const markDown = require('./utils/generateMarkdown');
+markDown
+
+const fs = require('node:fs/promises');
+
 
 // TODO: Create an array of questions for user input
-const questions = [];
+// User will be prompted to fill in this data which will display on their README
+const questions = [
+    {
+        type: 'input',
+        name: 'project title',
+        message: "What's the title of your project for your README?"
+    },
+
+    {
+        type: 'input',
+        text: 'description',
+        message: "Describe what your project is all about and how it works"
+    },
+
+    {
+        type: 'input',
+        text: 'installation',
+        message: "Provide instructions on how to install your app"
+    },
+
+    {
+        type: 'input',
+        text: 'usage',
+        message: ""
+    },
+
+    {
+        type: 'input',
+        text: 'contribution',
+        message: ""
+    },
+
+    {
+        type: 'input',
+        text: 'test',
+        message: ""
+    }
+
+];
+
+inquirer.prompt(questions).then((answers) => {
+    console.log(JSON.stringify(answers, null, '  '));
+});
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
 
+}
+writeToFile();
 // TODO: Create a function to initialize app
-function init() {}
+function init() { }
 
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,20 +1,62 @@
+// This is where all the README Markdown code will go
+// This Module will communicate with the index js module
+
+console.log(`Program is starting...`)
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) { // Section when you type license this is the function that will add a license badge
+
+  if (license) const noLicense = '';
+  return LicenseBadge;
+}
+
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) { }
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) { }
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  const template = `# ${data.title};
 
-`;
+  #Title of the Project
+  - [Description](#Description)
+  - [Table of Contents](#TableofContents)
+  - [Installation](#Installation)
+  - [Usage](#Usage)
+  - [License](#License)
+  - [Contributing](#Contributions)
+  - [Tests](#Tests)
+  - [Questions](#Questions)
+
+
+  #Title of the Project
+  ## Description
+  ${Description}
+  ## Table of Contents
+  ${TableofContents}
+  ## Installation
+  ${Installation}
+  ## Usage
+  ${Usage}
+  ## License
+  ${License}
+  ## Contributing
+  ${Contributions}
+  ## Tests
+  ${Tests}
+  ## Questions
+  ${Questions}
+
+
+
+  return `# ${ data.title }
+
+
 }
-
+console.log(`Program has ended...`)
 module.exports = generateMarkdown;
